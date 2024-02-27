b8eccf740695ebbd7d3f4f247788fc427507f0dd
vararais <varadigmaisnann@gmail.com>
2024-02-27 18:49:48 +0700

    Add project files.

---
+++ CobaCobi/CobaCobi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//Begin
+
+    //numeric nJejari, nLuas 
+    //display 'Masukan jejari= ,
+    //accept nJejari
+    //compute nLuas = 3.14 * r * r
+    //display 'Luasnya= ' + nLuas
+
+    float nJejari, nLuas;
+    cout << "Masukan jejari = ";
+    cin >> nJejari;
+    nLuas = 3.14 * nJejari * nJejari;
+    cout << "Luasnya = " << nLuas;
+
+}
